a80f93b963fc7bb0f3ea4338a5b535280baa3515
Ganesh Khaire <ganesh.khaire91@gmail.com>
2020-03-25 00:57:55 +0530

    Add files via upload

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -107,6 +107,7 @@ export class ArticleController {
   async createPost(
     @Body() article: SlackArticleDTO
   ): Promise<any> {
+    console.log("article",article)
     return await article;
   }
 
